481d2a1438036c926aedf4e2b02935119b6c3765
Apoorva Narayan <75483348+apoorvasubhi@users.noreply.github.com>
2026-05-12 13:01:41 +0530

    Cleaned project structure

--- check_models.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyAUUdqBtf9zFNNGZYOpGluVzEx9gOAevuI")
-
-models = genai.list_models()
-
-for model in models:
-    print(model.name)
